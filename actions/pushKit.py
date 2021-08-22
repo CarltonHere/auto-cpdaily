d6bb94ef793968c43dce3305b05cc556b10f2893
Carlton <icarlton@163.com>
2021-08-22 18:46:01 +0800

    优化推送服务配置项

--- actions/pushKit.py
+++ actions/pushKit.py
@@ -12,37 +12,48 @@ class pushKit:
         self.type = option['method']
 
     def sendMsg(self, title, msg, user=''):
-        if('notifyOption' in user):
-            userOption = user['notifyOption']
-        else:
+        if ('notifyOption' not in user):
             return '该用户未配置推送，已取消发送！'
+        if ('rcvAcc' not in user['notifyOption']):
+            return '该用户未配置推送，已取消发送！'
+        if (user['notifyOption']['rcvAcc'] == ""):
+            return '该用户未配置推送，已取消发送！'
+
+        userOption = user['notifyOption']
+        # 获取接收账号
+        rcvAcc = userOption['rcvAcc']
+        # 获取全局推送模式
         method = self.type
-        if('method' in userOption):
+        # 获取用户指定的推送模式
+        if ('method' in userOption):
             method = userOption['method']
+
         # 判断推送类型
-        if 'email' in userOption:
-            if method == 1:
-                return self.sendMsgByMailApi(userOption['email'], title, msg)
-            elif method == 2:
-                return self.sendMsgBySmtp(userOption['email'], title, msg)
-        if 'qId' in userOption:
-            if method == 3:
-                return self.sendMsgByQmsg(userOption['qId'], title, msg)
         if method == 0:
             return '消息推送服务未启用'
+        if method == 1:
+            return self.sendMsgByMailApi(rcvAcc, title, msg)
+        if method == 2:
+            return self.sendMsgBySmtp(rcvAcc, title, msg)
+        if method == 3:
+            return self.sendMsgByQmsg({'type': 0, 'id': rcvAcc}, title, msg)
         if method == 4:
-            return self.sendMsgByQyWx(userOption['qywx'], title, msg)
+            return self.sendMsgByQmsg({'type': 1, 'id': rcvAcc}, title, msg)
+        if method == 5:
+            return self.sendMsgByQyWx(rcvAcc, title, msg)
+
         return '推送参数配置错误，已取消发送！'
 
     # 发送邮件消息
     def sendMsgByMailApi(self, mail, title, msg):
         if mail == '':
             return '邮箱为空，已取消发送邮件！'
-        if self.option['apiUrl'] == '':
+        if self.option['mailApiUrl'] == '':
             return '邮件API为空，设置邮件API后才能发送邮件'
-        params = {'to': mail, 'title': title, 'content': msg}
-        res = requests.post(url=self.option['apiUrl'], params=params).json()
-        return res['msg']
+        # 以下部分需要根据不同接口自行修改
+        params = {'reciever': mail, 'title': title, 'content': msg}
+        res = requests.post(url=self.option['mailApiUrl'], params=params).json()
+        return res['message']
 
         # smtp本地邮件接口
     def sendMsgBySmtp(self, mail, title, msg):
@@ -103,8 +114,9 @@ class pushKit:
             return 'qmsg推送失败,' + res['reason']
 
     #企业微信应用推送
-    def sendMsgByQyWx(self, user_info, title, message):
+    def sendMsgByQyWx(self, rcvAcc, title, message):
         wxConfig = self.option['qywxOption']
+
         def get_access_token(wxConfig):
             get_token_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
             response = requests.get(get_token_url, params=wxConfig).json()
@@ -113,23 +125,29 @@ class pushKit:
             else:
                 print(response)
                 return '获取access_token失败'
+
         if wxConfig['corpid'] and wxConfig['corpsecret']:
             try:
                 access_token = get_access_token(wxConfig)
                 if isinstance(access_token, str):
-                    user_info['text'] = {'content': f'{title}:\n\n{message}'}
+                    params = {
+                        'touser': rcvAcc,
+                        "agentid": wxConfig['agentid'],
+                        "msgtype": "text",
+                        'text': {
+                            'content': f'{title}\n{message}'
+                        }
+                    }
                     url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
-                    response = requests.post(url=url, json=user_info).json()
+                    response = requests.post(url=url, json=params).json()
                     # print(response)
-                    return True if response['errmsg'] == 'ok' else response
+                    return '企业微信推送成功' if response[
+                        'errmsg'] == 'ok' else response
                 else:
                     print(access_token)
                     return access_token
             except Exception as e:
                 print('推送失败:', e)
-
-        elif self.method != 4:
-            pass
         else:
             return '企业微信应用配置错误，请检查qywxOption'
 
