23d678f042fa40c608d5d7544844ea6bb7a9db66
IceTiki <tiki_pufferfish@outlook.com>
2022-04-11 14:41:29 +0800

    删除(常驻化)debug
    使用Trackback(python标准模块)，进行出错位置定位输出
    
    特性新增：会推送出错位置

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ from actions.workLog import workLog
 from actions.pushKit import pushKit
 from actions.utils import Utils
 from time import sleep
+import traceback
 
 
 def main():
@@ -14,38 +15,37 @@ def main():
     push = pushKit(config['notifyOption'])
     httpProxy = config['httpProxy'] if 'httpProxy' in config else ''
     for user in config['users']:
-        Utils.log(
-            f"10s后开始执行用户{user['user']['username'] if user['user']['username'] else '默认用户'}的任务"
-        )
+        Utils.log(f"10s后开始执行用户{user['user'].get('username','默认用户')}的任务")
         sleep(10)
-        if config['debug']:
+
+        # 执行任务
+        try:
             msg = working(user, httpProxy)
-        else:
-            try:
-                msg = working(user, httpProxy)
-                ret = True
-            except Exception as e:
-                msg = str(e)
-                ret = False
-            ntm = Utils.getTimeStr()
-            if ret == True:
-                #此处需要注意就算提示成功也不一定是真的成功，以实际为准
-                Utils.log(msg)
-                if 'SUCCESS' in msg:
-                    msg = push.sendMsg(
-                        '今日校园签到成功通知',
-                        '服务器(V%s)于%s尝试签到成功!' % (config['Version'], ntm),
-                        user['user'])
-                else:
-                    msg = push.sendMsg(
-                        '今日校园签到异常通知', '服务器(V%s)于%s尝试签到异常!\n异常信息:%s' %
-                        (config['Version'], ntm, msg), user['user'])
-            else:
-                Utils.log("Error:" + msg)
-                msg = push.sendMsg(
-                    '今日校园签到失败通知', '服务器(V%s)于%s尝试签到失败!\n错误信息:%s' %
-                    (config['Version'], ntm, msg), user['user'])
+            ret = True
+        except Exception as e:
+            msg = f'[{e}]\n{traceback.format_exc()}'
+            ret = False
+
+        # 根据任务执行情况(ret和msg)进行推送和记录
+        ntm = Utils.getTimeStr()
+        if ret == True:
+            # 此处需要注意就算提示成功也不一定是真的成功，以实际为准
             Utils.log(msg)
+            if 'SUCCESS' in msg:
+                msg = push.sendMsg(
+                    '今日校园签到成功通知',
+                    '服务器(V%s)于%s尝试签到成功!' % (config['Version'], ntm),
+                    user['user'])
+            else:
+                msg = push.sendMsg(
+                    '今日校园签到异常通知', '服务器(V%s)于%s尝试签到异常!\n异常信息:%s' %
+                    (config['Version'], ntm, msg), user['user'])
+        else:
+            Utils.log("Error:" + msg)
+            msg = push.sendMsg(
+                '今日校园签到失败通知', '服务器(V%s)于%s尝试签到失败!\n错误信息:%s' %
+                (config['Version'], ntm, msg), user['user'])
+        Utils.log(msg)
     Utils.log("自动化任务执行完毕")
 
 
@@ -66,7 +66,7 @@ def working(user, httpProxy):
         sleep(1)
         msg = collection.submitForm()
         return msg
-    elif user['user']['type'] in [1,2,3]:
+    elif user['user']['type'] in [1, 2, 3]:
         # 以下代码是签到的代码
         Utils.log('开始执行签到任务')
         sign = AutoSign(wise, user['user'])
